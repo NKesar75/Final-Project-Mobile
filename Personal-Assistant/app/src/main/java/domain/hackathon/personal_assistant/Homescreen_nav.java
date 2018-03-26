3562cbcc77ccd0b880f04de8912a1bf899405921
Sean Ermish <SMErmish@fullsail.edu>
2018-03-26 09:28:03 -0400

    fixed bugs

--- Personal-Assistant/app/src/main/java/domain/hackathon/personal_assistant/Homescreen_nav.java
+++ Personal-Assistant/app/src/main/java/domain/hackathon/personal_assistant/Homescreen_nav.java
@@ -276,34 +276,3 @@ public class Homescreen_nav extends AppCompatActivity
 
 }
 
-
-//package org.norbert;
-//
-//        import java.io.*;
-//
-//public class PythonCaller {
-//
-//    /**
-//     * @param args
-//     * @throws IOException
-//     */
-//    public static void main(String[] args) throws IOException {
-//// set up the command and parameter
-//        String pythonScriptPath = "/home/norbert/python/helloPython.py";
-//        String[] cmd = new String[2];
-//        cmd[0] = "python"; // check version of installed python: python -V
-//        cmd[1] = pythonScriptPath;
-//
-//// create runtime to execute external command
-//        Runtime rt = Runtime.getRuntime();
-//        Process pr = rt.exec(cmd);
-//
-//// retrieve output from python script
-//        BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-//        String line = "";
-//        while((line = bfr.readLine()) != null) {
-//// display each output line form python script
-//            System.out.println(line);
-//        }
-//    }
-//}
